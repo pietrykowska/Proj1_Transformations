a062ddde8fe28a0fcea03b00050c6f28675db2be
WiktoriaSamek <wiktoria.samek@gmail.com>
2023-04-26 16:55:07 +0200

    Add definitions about ellipsoids

--- transformations.py
+++ transformations.py
@@ -1,2 +1,14 @@
 class Transformations:
-   
+    def __init__(self):
+       
+            self.ellipsoid = {
+                'GRS80': {'a': 6378137,
+                          'b': 6356752.3141},
+                        
+                'WGS84': {'a': 6378137,
+                          'b': 6356752.3142},
+                         
+                'KRASOWSKI': {'a': 6378245,
+                              'b': 6356863.019}}
+            
+       
