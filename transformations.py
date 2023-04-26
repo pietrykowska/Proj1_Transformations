84a4208811986eb405dd35d6ddb91544059af91b
WiktoriaSamek <wiktoria.samek@gmail.com>
2023-04-26 17:34:50 +0200

    add formula in def hirvonen

--- transformations.py
+++ transformations.py
@@ -1,3 +1,6 @@
+from math import *
+import numpy as np
+
 class Transformations:
     def __init__(self):
        
@@ -14,14 +17,35 @@ class Transformations:
                               'b': 6356863.019,
                               'e2': 0.00669342162297}}
                               
-#def XYZ2BLH
+
+    def hirvonen(self): 
+        a = self.ellipsoid['a']
+        e2 = self.ellipsoid['e2']
+        
+        p = np.sqrt(X**2 + Y**2)
+        f = np.arctan(Z / (p*(1 - e2)))
+        while True:
+            N = Np(f,a,e2)
+            h = (p/np.cos(f)) - N
+            fs = f
+            f = np.arctan(Z / (p * (1 - e2 * N / (N + h))))
+            if np.abs(fs - f) < (0.000001/206265):
+                break
+        l = np.arctan2(Y,X)
+        return(f,l,h)
+    
+        
     def BLH2XYZ(ellipsoid_name):
         a = self.ellipsoid['a']
         e2 = self.ellipsoid['e2']
 
-#def XYZ2NEU
-#def BL2XY2000
-#def BL2XY1992
-            
-    def hirvonen(): 
+    #def NEU():
+
+    #def XY2000():
         
+    #def XY1992():    
+
+        
+
+#if __name__ == '__main__':
+    
