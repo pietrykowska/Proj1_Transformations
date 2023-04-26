a97b8698181dc9c4d735aaacfc9e025176b34585
Patrycja <ppietrykowska26@gmail.com>
2023-04-26 19:44:08 +0200

    create a new branch and clause ifname==main

---
+++ arg.py
@@ -0,0 +1,18 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Transformation of coordinates')
+
+    parser.add_argument('-dat',
+                        type=str,
+                        help='If the file is located in the same folder, enter its name with the extension. If the file is located elsewhere, enter the path.')
+    parser.add_argument('-method',
+                        type=str,
+                        help='Accepts the name of the selected transformation (XYZ2BLH, BLH2XYZ, XYZ2NEU, BL2XY2000, BL2XY1992)')
+    parser.add_argument('-ellip',
+                        type=str,
+                        help='Accepts the ellipsoid model (WGS84/ GRS80/ KRASOWSKI)')
+
+    args = parser.parse_args()
+
+    end = ""
