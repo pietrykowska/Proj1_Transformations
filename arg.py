91db57de757c71061aec681337f74d2db0feddaf
Patrycja <ppietrykowska26@gmail.com>
2023-04-26 20:16:27 +0200

    add excepts

--- arg.py
+++ arg.py
@@ -23,11 +23,11 @@ if __name__ == '__main__':
                        'BL2XY1992': 'BL2XY1992'}
     end = ""
     try:
-        while end != "Finish":
+        while end != "THE END":
             if args.method == None:
-                args.method = input(str('Transformation name: '))
+                args.method = input(str('Transformation name: ')).upper()
             if args.ellip == None:
-                args.ellip = input(str('Ellipsoid model: '))
+                args.ellip = input(str('Ellipsoid model: ')).upper()
             if args.dat == None:
                 args.dat = input(str('Enter the path to the txt file with data: '))
 
@@ -43,7 +43,20 @@ if __name__ == '__main__':
                 data_out = sth.BL2XY2000(args.dat, args.ellip)
             if method == 'BL2XY1992':
                 data_out = sth.BL2XY1992(args.dat, args.ellip)
-                    
+
+            end = input(str("If you want to close the program, type - THE END, if you want to continue, type anything: ")).upper()
             args.ellip = None
             args.dat = None
-            args.method = None    
+            args.method = None
+
+    except FileNotFoundError:
+        print('File not found.')
+    except KeyError:
+        print('Incorrect program parameters.')
+    except IndexError:
+        print('Wrong data format.')
+    except ValueError:
+        print('Wrong data format.')
+    finally:
+        print(20 * '*')
+        print('THE END')
